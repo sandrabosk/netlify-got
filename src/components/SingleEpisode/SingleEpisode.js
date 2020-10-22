40c41f759bf4bfe53f93021334cb47fe57a89542
sandrabosk <sandraboskovic@hotmail.com>
2020-10-22 03:40:46 +0200

    add code

--- src/components/SingleEpisode/SingleEpisode.js
+++ src/components/SingleEpisode/SingleEpisode.js
@@ -9,20 +9,16 @@ class SingleEpisode extends React.Component {
     const summaryShort = summaryFull.slice(0, 70);
     return (
       <div className='card'>
-        <img
-          src={episode.image.medium}
-          className='card-img-top'
-          alt={episode.name}
-        />
+        <img src={episode.image.medium} className='card-img-top' alt={episode.name} />
         <div className='card-body'>
           <h5 className='card-title'>{episode.name}</h5>
           <h6 className='card-title'>
             S{episode.season} E{episode.number}
           </h6>
           <p className='card-text'>{summaryShort}</p>
-          <a href='#' className='btn btn-primary'>
+          {/* <a href='#' className='btn btn-primary'>
             Go somewhere
-          </a>
+          </a> */}
         </div>
       </div>
     );
